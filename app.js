4fb3a62017200de9c4fb09ef19caf7d3aa4e00d1
Irene <airine@Irenes-MacBook-Air.local>
2020-07-17 14:10:30 -0700

    [variables]add 5 variables

---
+++ app.js
@@ -0,0 +1,26 @@
+// 5 variable of difference datatype
+//string
+//number
+//array
+//boolean
+//object (4 different key/value pairs)[string],[number],[array],[boolean]
+
+let sentences = 'Hello World'
+console.log(sentences)
+
+var num = 22
+console.log(num)
+
+const foods = ['Chicken', 'Vegetable', 'Pizza', 'Fruits', 'Burger']
+console.log(foods)
+
+let itWork=  true;
+console.log(itWork)
+
+const lakers = {
+    headquarters: 'Los Angeles',
+    numberOfPlayers: 11,
+    players: ['Anthony', 'John', 'David'],
+    areGood: true
+}
+console.log(lakers)
